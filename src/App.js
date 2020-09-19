ad9fa5a30aa05e6fca6e16fc233635dbe68c881d
Phyllis Kan <pyk9q7@gmail.com>
2020-09-19 17:26:24 -0500

    fix body layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom'
 // import TabPanel from './components/layout/Navigation'
 // import ColumnSize from './components/layout/ThemeStyles'
-import MenuBar from './components/layout/Menu'
+import MenuBar from './components/layout/MenuBar'
+import Container from '@material-ui/core/Container'
 import "./app.css"
 import Home from './components/home/Home'
 import About from './components/about/About'
@@ -13,12 +14,15 @@ const App = () => {
     <div className="page-container">
       <div>
         <MenuBar />
-        <Switch>
-          <Redirect exact from="/Portfolio/" to="/Portfolio/home" />
-          <Route exact path="/Portfolio/home" render={props => <Home {...props} />} />
-          <Route exact path="/Portfolio/about" render={props => <About {...props} />} />
-          {/* <Route exact path="/Portfolio/:page?" render={props => <MenuBar {...props} ColumnSize={ColumnSize} />} /> */}
-        </Switch>
+        <Container maxWidth="md">
+          <Switch>
+            <Redirect exact from="/Portfolio/" to="/Portfolio/home" />
+            <Route exact path="/Portfolio/home" render={props => <Home {...props} />} />
+            <Route exact path="/Portfolio/about" render={props => <About {...props} />} />
+            {/* <Route exact path="/Portfolio/:page?" render={props => <MenuBar {...props} ColumnSize={ColumnSize} />} /> */}
+          </Switch>
+        </Container>
+
       </div>
     </div>
   )
